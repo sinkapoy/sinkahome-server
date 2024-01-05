013caef688495abc284b3ca1507019543aec789c
sinkapoy <sharshov-ivan@yandex.ru>
2024-01-05 16:32:15 +0300

    update package @sinkapoy/home-integrations-sinkapoy-serial

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 
 import { configureNetworking } from "@sinkapoy/home-integrations-networking";
 import { homeEngine } from "@sinkapoy/home-core";
-import { configureSinkapoySerialModule } from "@sinkapoy/home-integrations-sinkapoy-serial";
+import "@sinkapoy/home-integrations-sinkapoy-serial";
 import "@sinkapoy/home-integrations-server-widgets";
 import "@sinkapoy/home-integrations-scripts";
 import "./ecs/systems/DebugSystem";
 
 configureNetworking({port: 18956});
-configureSinkapoySerialModule();
 
 let prevTime = Date.now();
 const tick = async () => {
