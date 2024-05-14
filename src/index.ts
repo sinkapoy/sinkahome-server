f16b01ef1eb54920b1de8a7f98bb419ea40557bc
sinkapoy <sharshov-ivan@yandex.ru>
2024-05-14 23:08:04 +0300

    add user-names system

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+import { EntityUserNameSystem } from "./ecs/systems/EntityUserNameSystem";
 import { configureNetworking } from "@sinkapoy/home-integrations-networking";
 import { homeEngine } from "@sinkapoy/home-core";
 import "@sinkapoy/home-integrations-sinkapoy-serial";
@@ -10,6 +10,7 @@ import "@sinkapoy/home-integrations-zigbee";
 
 import "@sinkapoy/home-integrations-air-systems";
 configureNetworking({port: 18956});
+homeEngine.addSystem(new EntityUserNameSystem(), Number.MAX_SAFE_INTEGER);
 
 let prevTime = Date.now();
 const tick = async () => {
